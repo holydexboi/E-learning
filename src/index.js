2b1f7c9a7a4e646b29eea2d81cb9fe0efbe6bbba
holydexboi <holydexboi@yahoo.com>
2023-12-03 16:36:56 +0100

    change port

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ require("./routes/registerCourse")(app);
 require("./routes/test")(app);
 require("./routes/userTest")(app);
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
