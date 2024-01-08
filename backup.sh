49967d41d068ebb49939ea94b3e481d9106acd19
holydexboi <holydexboi@yahoo.com>
2024-01-08 12:51:53 +0100

    change dir

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ DATABASE='elearning'
 echo "Pulling Database: This may take a few minutes"
 
 # Set the folder where the database backup will be stored
-backupfolder=/root/Projects/E-learning/backup
+backupfolder=/root/Projects/E-learning/uploads
 
 sqlfile=$backupfolder/elearrning.sql
 zipfile=$backupfolder/elearning.gz
