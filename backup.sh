0547b3455b9fdef743cae469a85468c5f03c4e88
holydexboi <holydexboi@yahoo.com>
2024-01-09 10:33:28 +0100

    backup for upload'

--- backup.sh
+++ backup.sh
@@ -10,13 +10,24 @@ echo "Pulling Database: This may take a few minutes"
 # Set the folder where the database backup will be stored
 backupfolder=/root/Projects/E-learning/backup
 
+uploadFolder=/root/Projects/E-learning/uploads
+
 sqlfile=$backupfolder/elearning.sql
 
+resourcesfile=$backupfolder/upload.zip
 
+rm $sqlfile
 if mysqldump  $DATABASE > $sqlfile ; then
    echo 'Sql dump created'
     
 else
    echo 'pg_dump return non-zero code No backup was created!' 
    exit
+fi
+rm $resourcesfile
+if zip -r $resourcesfile $uploadFolder; then
+    echo 'The backup was successfully compressed'
+else
+    echo 'Error compressing backupBackup was not created'
+    exit
 fi
